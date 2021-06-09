e72b2c2b1cee0778b6a1c255807d4bbcf98cb4a5
Rafael sanchez <eduardoxlau@hotmail.com>
2021-06-09 17:19:18 -0500

    validate folder empty

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 const regExp = process.argv.slice(2)[0];
 const rootPath = "./";
-const folders = [];
+let folders = [];
 
 const getFullPath = (path = rootPath) => {
   const isRootPath = path === rootPath;
@@ -10,23 +10,24 @@ const getFullPath = (path = rootPath) => {
   const files = fs.readdirSync(path, {
     withFileTypes: true,
   });
-
-  files.forEach((file) => {
-    if (file.isDirectory()) {
-      folders.push(path + file.name + "/");
-      getFullPath(folders[folders.length - 1]);
-    } else {
-      if (new RegExp(regExp).test(file.name)) {
-        if (isRootPath) {
-          folders.push(rootPath + file.name);
-        } else {
-          folders[folders.length - 1] = path + file.name;
-        }
+  if (files.length > 0)
+    files.forEach((file) => {
+      if (file.isDirectory()) {
+        folders.push(path + file.name + "/");
+        getFullPath(folders[folders.length - 1]);
       } else {
-        folders.pop();
+        if (new RegExp(regExp).test(file.name)) {
+          if (isRootPath) {
+            folders.push(rootPath + file.name);
+          } else {
+            folders[folders.length - 1] = path + file.name;
+          }
+        } else {
+          folders.pop();
+        }
       }
-    }
-  });
+    });
+  else folders = [];
 
   return folders;
 };
