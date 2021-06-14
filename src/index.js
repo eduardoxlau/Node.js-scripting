e95218628605c66c48022ee00536d1950089900d
rafael sanchez <rafaelsanchez@rafaels-MacBook-Pro.local>
2021-06-14 09:55:36 -0500

    remove unnecesary files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var scripting_1 = require("./scripting");
-var regExp = process.argv.slice(2)[1];
-var rootPath = "./folders";
-var files = scripting_1.getPath(rootPath);
-scripting_1.validatRegExp(files, regExp).forEach(function (file) { return console.log(file); });
